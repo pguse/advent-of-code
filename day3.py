2a6f9779f38cc5fc05bbe8a1030861f75c7c6d2f
pguse <pguse@albertcollege.ca>
2017-12-04 23:43:36 -0500

    finished day 3

--- day3.py
+++ day3.py
@@ -19,6 +19,32 @@ def manhatten(n):
         sign = sign*(-1)
     return abs(x) + abs(y)
 
+def sumNeighbours(n,size):
+    def total(g,i,j):
+        return g[i][j-1]+g[i][j+1]+g[i-1][j-1]+g[i-1][j]+g[i-1][j+1]+g[i+1][j-1]+g[i+1][j]+g[i+1][j+1]
+    grid = []
+    for i in range(size):
+        grid.append([0]*size)
+    row = size//2
+    col = size//2
+    grid[row][col] = 1
+    step = 1
+    stage = 1
+    while grid[row][col] <= n:
+        count = stage
+        while count > 0 and grid[row][col] <= n:
+            col = col + step
+            grid[row][col] = total(grid,row,col)
+            count -= 1
+        count = stage
+        while count > 0 and grid[row][col] <= n:
+            row = row - step
+            grid[row][col] = total(grid,row,col)
+            count -= 1
+        stage += 1
+        step *= -1
+    return grid[row][col]
+
 def test(f, s, result):
     if f(s) != result:
         return False
@@ -30,3 +56,5 @@ print( test(manhatten, 12, 3) )
 print( test(manhatten, 23, 2) )
 print( test(manhatten, 1024, 31) )
 print( manhatten(312051) )
+
+print(sumNeighbours(312051,50))
